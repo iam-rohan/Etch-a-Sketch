70b58f0e0a8d8806e3c8051de1c4b1feac41eaa6
iam-rohan <rohanacharya42@gmail.com>
2024-05-06 21:45:05 +0545

    Integrate every function into setStage with resetting grid ability

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const divMaker = document.querySelector(".container");
 
-const gridSize = 16;
-
-for (i = 0; i < gridSize; i++) {
-  for (j = 0; j < gridSize; j++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    divMaker.appendChild(div);
+function clearContainer() {
+  while (divMaker.firstChild) {
+    divMaker.removeChild(divMaker.firstChild);
   }
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
+function setStage() {
+  clearContainer();
+  let gridSize = prompt("How many pixels per side?");
+  let gridChecked = Math.min(parseInt(gridSize), 100);
+  const containerWidth = gridChecked * 20; // Assuming each grid item is 50px wide
+  const containerHeight = gridChecked * 20; // Assuming each grid item is 50px high
 
-gridItems.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    item.classList.add("grid-hover");
+  divMaker.style.width = containerWidth + "px";
+  divMaker.style.height = containerHeight + "px";
+  for (i = 0; i < gridChecked; i++) {
+    const outerDiv = document.createElement("div");
+
+    for (j = 0; j < gridChecked; j++) {
+      const div = document.createElement("div");
+      div.classList.add("grid-item");
+      outerDiv.appendChild(div);
+    }
+    outerDiv.classList.add("grid-row");
+    divMaker.appendChild(outerDiv);
+  }
+  const gridItems = document.querySelectorAll(".grid-item");
+
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.classList.add("grid-hover");
+    });
   });
-});
+}
