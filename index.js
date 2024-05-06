d23273d100c67559e1a98665f00aba33ecb087a6
iam-rohan <rohanacharya42@gmail.com>
2024-05-06 23:22:23 +0545

    Make the container dimension fixed and add a clear the canvas feature

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ function clearContainer() {
 
 function setStage() {
   clearContainer();
+
   let gridSize = prompt("How many pixels per side?");
   let gridChecked = Math.min(parseInt(gridSize), 100);
-  const containerWidth = gridChecked * 20; // Assuming each grid item is 50px wide
-  const containerHeight = gridChecked * 20; // Assuming each grid item is 50px high
 
-  divMaker.style.width = containerWidth + "px";
-  divMaker.style.height = containerHeight + "px";
+  const mainContainer = document.querySelector(".container");
+
+  mainContainer.style.width = "600px";
+  const boxWidth = mainContainer.offsetWidth / gridSize;
+
   for (i = 0; i < gridChecked; i++) {
     const outerDiv = document.createElement("div");
 
     for (j = 0; j < gridChecked; j++) {
       const div = document.createElement("div");
       div.classList.add("grid-item");
+      div.style.width = boxWidth + "px";
+      div.style.height = boxWidth + "px";
       outerDiv.appendChild(div);
     }
     outerDiv.classList.add("grid-row");
@@ -32,5 +36,12 @@ function setStage() {
     item.addEventListener("mouseover", () => {
       item.classList.add("grid-hover");
     });
+  });
+}
+
+function cleared() {
+  let gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => {
+    item.classList.remove("grid-hover");
   });
 }
